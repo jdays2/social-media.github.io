2459ab32efa526a72eb96a9c8e29a0420410a477
MarkHW <lolka-pop@rambler.ru>
2023-01-22 03:10:53 +0300

    More folders, moduls, and components

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,15 +1,12 @@
 import s from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 function Profile() {
   return (
-    <div className={s.cont}>
-      <img src="https://oir.mobi/uploads/posts/2021-03/1616601614_22-p-fon-kosmos-zvezdi-28.jpg" />
-
-      <div>
-        ava+discr
-        <MyPosts />
-      </div>
+    <div>
+      <ProfileInfo />
+      <MyPosts />
     </div>
   );
 }

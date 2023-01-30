6f7ac3a2b120975a8948076572696ff15e8f09ee
MarkHW <lolka-pop@rambler.ru>
2023-01-30 03:39:59 +0300

    Переход users на классовый компонент, ж.цикл

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,62 +1,66 @@
 import axios from "axios";
+import React from "react";
 import s from "./Users.module.css";
-function Users({ users, followUser, unfollowUser, setUsers }) {
-  if (users.length === 0) {
+
+class Users extends React.Component {
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => setUsers(response.data.items));
+      .then((response) => this.props.setUsers(response.data.items));
   }
-  console.log(users);
-  return (
-    <div className={s.wrapper}>
-      {users.map((u) => {
-        return (
-          <div className={s.userCard}>
-            <span>
-              <div className={s.userLogo}>
-                <img
-                  className={s.img}
-                  src={
-                    u.photos.small === null
-                      ? "https://mustact.by/img/empty/artist.avatar.png"
-                      : u.photos.small
-                  }
-                />
 
-                {u.followed ? (
-                  <button
-                    onClick={() => {
-                      unfollowUser(u.id);
-                    }}
-                  >
-                    Unfollow
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      followUser(u.id);
-                    }}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
-            </span>
-            <span>
+  render() {
+    return (
+      <div className={s.wrapper}>
+        {this.props.users.map((u) => {
+          return (
+            <div className={s.userCard}>
               <span>
-                <div>{u.name}</div>
-                <div>{u.status}</div>
+                <div className={s.userLogo}>
+                  <img
+                    className={s.img}
+                    src={
+                      u.photos.small === null
+                        ? "https://mustact.by/img/empty/artist.avatar.png"
+                        : u.photos.small
+                    }
+                  />
+
+                  {u.followed ? (
+                    <button
+                      onClick={() => {
+                        this.props.unfollowUser(u.id);
+                      }}
+                    >
+                      Unfollow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        this.props.followUser(u.id);
+                      }}
+                    >
+                      Follow
+                    </button>
+                  )}
+                </div>
               </span>
               <span>
-                <div>{"u.location.city"}</div>
-                <div>{"u.location.country"}</div>
+                <span>
+                  <div>{u.name}</div>
+                  <div>{u.status}</div>
+                </span>
+                <span>
+                  <div>{"u.location.city"}</div>
+                  <div>{"u.location.country"}</div>
+                </span>
               </span>
-            </span>
-          </div>
-        );
-      })}
-    </div>
-  );
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Users;

e5d2bc3081b9c08a451035a178ff3acb120b6197
MarkHW <lolka-pop@rambler.ru>
2023-01-30 15:50:59 +0300

    create and use preloader with svg

--- src/Redux/usersPageReducer.js
+++ src/Redux/usersPageReducer.js
@@ -2,12 +2,14 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 
 let initialState = {
   users: [],
   pageSize: 4,
   totalUserCount: 30, //Беру из головы, вместо api, потому как там totalUserCount > 22k (T_T)
   currentPage: 1,
+  isFetching: false,
 };
 
 const usersPageReducer = (state = initialState, action) => {
@@ -38,6 +40,8 @@ const usersPageReducer = (state = initialState, action) => {
 
     case SET_CURRENT_PAGE:
       return { ...state, currentPage: action.currentPage };
+    case TOGGLE_IS_FETCHING:
+      return { ...state, isFetching: action.isFetching };
 
     default:
       return state;
@@ -69,6 +73,13 @@ export const setCurrentPageAC = (currentPage) => {
   return {
     type: SET_CURRENT_PAGE,
     currentPage,
+  };
+};
+
+export const setToggleIsFetchingAC = (isFetching) => {
+  return {
+    type: TOGGLE_IS_FETCHING,
+    isFetching,
   };
 };
 
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -4,10 +4,12 @@ import {
   unfollowAC,
   setUsersAC,
   setCurrentPageAC,
+  setToggleIsFetchingAC,
 } from "../../Redux/usersPageReducer";
 import axios from "axios";
 import React from "react";
 import Users from "./Users";
+import preloader from "./../../assets/images/preloader.svg";
 
 const mapStateToProps = (state) => {
   return {
@@ -15,6 +17,7 @@ const mapStateToProps = (state) => {
     pageSize: state.usersPage.pageSize,
     totalUserCount: state.usersPage.totalUserCount,
     currentPage: state.usersPage.currentPage,
+    isFetching: state.usersPage.isFetching,
   };
 };
 
@@ -32,38 +35,56 @@ const mapDispatchToProps = (dispatch) => {
     setCurrentPage: (currentPage) => {
       dispatch(setCurrentPageAC(currentPage));
     },
+    toggleIsFetching: (isFetching) => {
+      dispatch(setToggleIsFetchingAC(isFetching));
+    },
   };
 };
 
 class UsersContainer extends React.Component {
   componentDidMount() {
+    this.props.toggleIsFetching(true);
+
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${this.props.currentPage}`
       )
-      .then((response) => this.props.setUsers(response.data.items));
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        this.props.setUsers(response.data.items);
+      });
   }
 
   onChangedPage = (pageNumber) => {
+    this.props.toggleIsFetching(true);
+    console.log(this.props.isFetching);
     this.props.setCurrentPage(pageNumber);
+
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?count=${this.props.pageSize}&page=${pageNumber}`
       )
-      .then((response) => this.props.setUsers(response.data.items));
+      .then((response) => {
+        this.props.toggleIsFetching(false);
+        console.log(this.props.isFetching);
+        this.props.setUsers(response.data.items);
+      });
   };
 
   render() {
     return (
-      <Users
-        onChangedPage={this.onChangedPage}
-        unfollowUser={this.props.unfollowUser}
-        followUser={this.props.followUser}
-        totalUserCount={this.props.totalUserCount}
-        pageSize={this.props.pageSize}
-        users={this.props.users}
-        currentPage={this.props.currentPage}
-      />
+      <>
+        {this.props.isFetching ? <img src={preloader} /> : null}
+        <Users
+          onChangedPage={this.onChangedPage}
+          unfollowUser={this.props.unfollowUser}
+          followUser={this.props.followUser}
+          totalUserCount={this.props.totalUserCount}
+          pageSize={this.props.pageSize}
+          users={this.props.users}
+          currentPage={this.props.currentPage}
+        />
+      </>
     );
   }
 }

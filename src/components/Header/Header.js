fc22838a744f608c3ed324be63e653550994c609
MarkHW <lolka-pop@rambler.ru>
2023-01-31 16:44:52 +0300

    refact

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,14 @@
 import s from "./Header.module.css";
+import loginImg from "./../../assets/images/login.svg";
 
 function Header() {
   return (
     <header className={s.header}>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/512px-SVG_Logo.svg.png" />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/SVG_Logo.svg/512px-SVG_Logo.svg.png"
+        className={s.logo}
+      />
+      <img src={loginImg} className={s.loginImg} />
     </header>
   );
 }
